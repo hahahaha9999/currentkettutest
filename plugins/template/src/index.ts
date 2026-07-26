80cb6c29fb76cca3c6534597af81446a6804a94a
hahahaha9999 <noreplyrobloxinfo.com@gmail.com>
2026-07-25 23:07:02 -0400

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,49 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { patcher, metro, storage, components } from "@vendetta";
+
+const { Forms } = components;
+const { React } = metro.common;
+const FluxDispatcher = metro.common.FluxDispatcher;
+
+storage.targetMessageId ??= "1520914436460904678";
+storage.spoofedText ??= "";
+storage.spoofedDisplayName ??= ".𝚔𝚊𝚣𝚏𝚕𝚊";
+storage.spoofedAvatar ??= "https://cdn.discordapp.com/avatars/758731615265357824/d58a1012427825b24816247844152b6a.png";
+
+let patches = [];
+
+function applyMobileSpoof(messageObj: any) {
+    if (!messageObj) return;
+    Object.defineProperty(messageObj, "content", { get: () => storage.spoofedText, set: () => {}, configurable: true });
+    if (messageObj.author) {
+        Object.defineProperty(messageObj.author, "username", { get: () => storage.spoofedDisplayName, configurable: true });
+        Object.defineProperty(messageObj.author, "globalName", { get: () => storage.spoofedDisplayName, configurable: true });
+        Object.defineProperty(messageObj.author, "avatar", { get: () => "spoofed", configurable: true });
+    }
+}
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        patches.push(patcher.before("dispatch", FluxDispatcher, (args) => {
+            const [payload] = args;
+            if ((payload.type === "LOAD_MESSAGES_SUCCESS" || payload.type === "LOCAL_MESSAGE_CREATE") && payload.messages) {
+                const target = payload.messages.find((m: any) => m?.id === storage.targetMessageId);
+                if (target) applyMobileSpoof(target);
+            }
+        }));
+        
+        const RowManager = metro.findByProps("prototype", "generate");
+        if (RowManager?.prototype) {
+            patches.push(patcher.after("generate", RowManager.prototype, (args, ret) => {
+                if (ret?.message?.id === storage.targetMessageId) applyMobileSpoof(ret.message);
+            }));
+        }
     },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+    onUnload: () => patches.forEach(p => p())
+};
+
+export const settings = () => React.createElement(Forms.FormSection, { title: "Spoof Settings" },
+    React.createElement(Forms.FormTextInput, { title: "Target Message ID", value: storage.targetMessageId, onChange: (v: string) => storage.targetMessageId = v }),
+    React.createElement(Forms.FormTextInput, { title: "Spoofed Display Name", value: storage.spoofedDisplayName, onChange: (v: string) => storage.spoofedDisplayName = v }),
+    React.createElement(Forms.FormTextInput, { title: "Spoofed Text Content", value: storage.spoofedText, onChange: (v: string) => storage.spoofedText = v }),
+    React.createElement(Forms.FormTextInput, { title: "Avatar URL", value: storage.spoofedAvatar, onChange: (v: string) => storage.spoofedAvatar = v })
+);
