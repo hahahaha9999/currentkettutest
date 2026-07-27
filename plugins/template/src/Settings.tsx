863e147e7ce98e3b1327cccd27f8241c5327a3f4
hahahaha9999 <noreplyrobloxinfo.com@gmail.com>
2026-07-27 10:42:32 -0400

    Update Settings.tsx

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,34 @@
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { storage } from "@vendetta";
+import { useProxy } from "@vendetta/storage";
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+const { FormSection, FormInput } = Forms;
+
+export default () => {
+    useProxy(storage);
+
+    return (
+        <FormSection title="Spoof Settings">
+            <FormInput
+                title="Target Message ID"
+                value={storage.targetMessageId}
+                onChange={(v: string) => (storage.targetMessageId = v)}
+            />
+            <FormInput
+                title="Spoofed Display Name"
+                value={storage.spoofedDisplayName}
+                onChange={(v: string) => (storage.spoofedDisplayName = v)}
+            />
+            <FormInput
+                title="Spoofed Text Content"
+                value={storage.spoofedText}
+                onChange={(v: string) => (storage.spoofedText = v)}
+            />
+            <FormInput
+                title="Avatar URL"
+                value={storage.spoofedAvatar}
+                onChange={(v: string) => (storage.spoofedAvatar = v)}
+            />
+        </FormSection>
+    );
+};
