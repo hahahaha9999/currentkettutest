53fecf88ff11f2fcfc9228a983ab66bfe0dfcdfe
hahahaha9999 <noreplyrobloxinfo.com@gmail.com>
2026-07-27 12:49:45 -0400

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -6,17 +6,38 @@ import Settings from "./Settings";
 storage.targetMessageId ??= "";
 storage.spoofedText ??= "";
 storage.spoofedDisplayName ??= "";
-storage.spoofedAvatar ??= "";
+storage.spoofedDecoAsset ??= "";
+storage.spoofedDecoSku ??= "";
 
 let patches = [];
 
 function applyMobileSpoof(messageObj: any) {
     if (!messageObj) return;
-    Object.defineProperty(messageObj, "content", { get: () => storage.spoofedText, set: () => {}, configurable: true });
+
+    // Spoof Text Content
+    if (storage.spoofedText) {
+        Object.defineProperty(messageObj, "content", { get: () => storage.spoofedText, set: () => {}, configurable: true });
+    }
+
     if (messageObj.author) {
-        Object.defineProperty(messageObj.author, "username", { get: () => storage.spoofedDisplayName, configurable: true });
-        Object.defineProperty(messageObj.author, "globalName", { get: () => storage.spoofedDisplayName, configurable: true });
-        Object.defineProperty(messageObj.author, "avatar", { get: () => "spoofed", configurable: true });
+        // Spoof Display Name
+        if (storage.spoofedDisplayName) {
+            Object.defineProperty(messageObj.author, "username", { get: () => storage.spoofedDisplayName, configurable: true });
+            Object.defineProperty(messageObj.author, "globalName", { get: () => storage.spoofedDisplayName, configurable: true });
+            Object.defineProperty(messageObj.author, "nick", { get: () => storage.spoofedDisplayName, configurable: true });
+        }
+
+        // Spoof Avatar Decoration (Asset Hash + SKU ID)
+        if (storage.spoofedDecoAsset && storage.spoofedDecoSku) {
+            const decoObj = {
+                asset: storage.spoofedDecoAsset,
+                skuId: storage.spoofedDecoSku,
+                sku_id: storage.spoofedDecoSku
+            };
+
+            Object.defineProperty(messageObj.author, "avatarDecoration", { get: () => decoObj, configurable: true });
+            Object.defineProperty(messageObj.author, "avatarDecorationData", { get: () => decoObj, configurable: true });
+        }
     }
 }
 
@@ -25,7 +46,7 @@ export default {
         if (FluxDispatcher) {
             patches.push(patcher.before("dispatch", FluxDispatcher, (args) => {
                 const [payload] = args;
-                if ((payload.type === "LOAD_MESSAGES_SUCCESS" || payload.type === "LOCAL_MESSAGE_CREATE") && payload.messages) {
+                if ((payload?.type === "LOAD_MESSAGES_SUCCESS" || payload?.type === "LOCAL_MESSAGE_CREATE") && payload.messages) {
                     const target = payload.messages.find((m: any) => m?.id === storage.targetMessageId);
                     if (target) applyMobileSpoof(target);
                 }
