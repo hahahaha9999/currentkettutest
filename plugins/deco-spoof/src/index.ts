444aa6293713e233c2db4fa396529465fa1feef9
hahahaha9999 <noreplyrobloxinfo.com@gmail.com>
2026-07-28 07:17:56 -0400

    Update index.ts

--- plugins/deco-spoof/src/index.ts
+++ plugins/deco-spoof/src/index.ts
@@ -8,18 +8,16 @@ let patches: any[] = [];
 
 export default {
     onLoad: () => {
-        // Patch user profile/messages to force your custom decoration
         const UserStore = metro.findByProps("getCurrentUser");
         if (UserStore) {
-            patches.push(patcher.after("getCurrentUser", UserStore, () => {
-                if (storage.spoofedDecoration) {
-                    return {
-                        avatarDecorationData: {
-                            asset: storage.spoofedDecoration,
-                            skuId: "000000000000000000"
-                        }
+            patches.push(patcher.after("getCurrentUser", UserStore, (_, ret) => {
+                if (ret && storage.spoofedDecoration) {
+                    ret.avatarDecorationData = {
+                        asset: storage.spoofedDecoration,
+                        sku_id: "000000000000000000"
                     };
                 }
+                return ret;
             }));
         }
     },
